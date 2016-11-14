add0d3d04fd56f1847bcc29949230ea9869385e4
li3097 <l.irby@aol.com>
2016-11-14 09:44:50 -0700

    11/14/2016

---
+++ Get Started part 1.js
@@ -0,0 +1,72 @@
+// This is a comment that the computer will ignore. 
+// It is for your eyes only!
+"cake".length
+
+// Also try the Q&A forum to get help
+// The link is on the bottom left of the page!
+confirm("I feel awesome!");
+
+prompt("Where are you from?");
+
+"What is your favorite song?"
+"string".length
+
+"I'm coding like a champ".length
+length < 10
+
+console.log(2 * 5)
+console.log("Hello")
+
+// Here is an example of using the greater than (>) operator.
+console.log(15 > 4); // 15 > 4 evaluates to true, so true is printed.
+
+// Fill in with >, <, === so that the following print out true:
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2 === 16);
+
+if (5<7 ) {
+    console.log("35 is amazing" );
+}
+
+if (5>7 ) 
+{
+    console.log("Let's go down the first road!");
+}
+else 
+{
+    // What should we do if the condition is false? Fill in here:
+    console.log("We are going to turn around!");
+}
+
+// Remember, the order and punctuation matter.
+// If you get an error, check carefully, line by line.
+// If you're really stuck, click "Stuck? Get a hint!"
+
+if (9<4)
+{console.log("The condition is true");
+}
+else
+{console.log("The condition is false");
+}
+
+// The computer doesn't worry about extra spaces between words or brackets
+// It just cares about the order of where things are placed
+// and that you have used the right characters (){}[]"";
+
+// For extra help, a program called a 'linter' is checking your code
+// and will put a red 'x' next to the first line that contains errors
+
+if (10 > 9) 
+{console.log("You got a true!");
+} 
+else 
+{console.log("You got a false!");
+}
+
+if ("Jon".length * 2 / (2+1) === 2)
+{console.log("The answer makes sense!");
+} 
+else 
+{ console.log("Error Error Error");
+}
